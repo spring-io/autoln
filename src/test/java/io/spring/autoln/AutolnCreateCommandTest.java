cefc9ddd70eb916936e3a2e0710d7c5a2cdd6e9e
Rob Winch <rwinch@users.noreply.github.com>
2020-10-14 09:41:48 -0500

    Additional assertions for symlink creation

--- src/test/java/io/spring/autoln/AutolnCreateCommandTest.java
+++ src/test/java/io/spring/autoln/AutolnCreateCommandTest.java
@@ -94,33 +94,30 @@ class AutolnCreateCommandTest {
 		assertThatPath(springFrameworkProject.resolve("1.0.x")).isRelativeSymlinkTo("1.0.2");
 		assertThatPath(springFrameworkProject.resolve("1.1.x")).isRelativeSymlinkTo("1.1.5");
 		assertThatPath(springFrameworkProject.resolve("1.2.x")).isRelativeSymlinkTo("1.2.9");
-//		"src/test/resources/docs/spring-framework/1.0.x => 1.0.2\n" +
-//				"src/test/resources/docs/spring-framework/1.1.x => 1.1.5\n" +
-//				"src/test/resources/docs/spring-framework/1.2.x => 1.2.9\n" +
-//				"src/test/resources/docs/spring-framework/2.0.x => 2.0.8\n" +
-//				"src/test/resources/docs/spring-framework/2.5.x => 2.5.6\n" +
-//				"src/test/resources/docs/spring-framework/3.0.x => 3.0.7.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/3.1.x => 3.1.4.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/3.2.x => 3.2.18.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/3.2.x-SNAPSHOT => 3.2.19.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/3.3.x-SNAPSHOT => 3.3.0.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/4.0.x => 4.0.9.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/4.1.x => 4.1.9.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/4.1.x-SNAPSHOT => 4.1.10.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/4.2.x => 4.2.9.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/4.2.x-SNAPSHOT => 4.2.10.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/4.3.x => 4.3.29.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/4.3.x-SNAPSHOT => 4.3.30.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/5.0.x => 5.0.19.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/5.0.x-SNAPSHOT => 5.0.20.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/5.1.x => 5.1.18.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/5.1.x-SNAPSHOT => 5.1.19.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/5.2.x => 5.2.9.RELEASE\n" +
-//				"src/test/resources/docs/spring-framework/5.2.x-SNAPSHOT => 5.2.10.BUILD-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/5.3.x => 5.3.0-RC1\n" +
-//				"src/test/resources/docs/spring-framework/5.3.x-SNAPSHOT => 5.3.0-SNAPSHOT\n" +
-//				"src/test/resources/docs/spring-framework/current => 5.3.0-RC1\n" +
-//				"src/test/resources/docs/spring-framework/current-SNAPSHOT => 5.3.0-SNAPSHOT\n");
+		assertThatPath(springFrameworkProject.resolve("2.0.x")).isRelativeSymlinkTo("2.0.8");
+		assertThatPath(springFrameworkProject.resolve("2.5.x")).isRelativeSymlinkTo("2.5.6");
+		assertThatPath(springFrameworkProject.resolve("3.0.x")).isRelativeSymlinkTo("3.0.7.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("3.1.x")).isRelativeSymlinkTo("3.1.4.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("3.2.x")).isRelativeSymlinkTo("3.2.18.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("3.2.x-SNAPSHOT")).isRelativeSymlinkTo("3.2.19.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("3.3.x-SNAPSHOT")).isRelativeSymlinkTo("3.3.0.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("4.0.x")).isRelativeSymlinkTo("4.0.9.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("4.1.x")).isRelativeSymlinkTo("4.1.9.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("4.1.x-SNAPSHOT")).isRelativeSymlinkTo("4.1.10.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("4.2.x")).isRelativeSymlinkTo("4.2.9.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("4.2.x-SNAPSHOT")).isRelativeSymlinkTo("4.2.10.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("4.3.x")).isRelativeSymlinkTo("4.3.29.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("4.3.x-SNAPSHOT")).isRelativeSymlinkTo("4.3.30.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("5.0.x")).isRelativeSymlinkTo("5.0.19.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("5.0.x-SNAPSHOT")).isRelativeSymlinkTo("5.0.20.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("5.1.x")).isRelativeSymlinkTo("5.1.18.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("5.1.x-SNAPSHOT")).isRelativeSymlinkTo("5.1.19.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("5.2.x")).isRelativeSymlinkTo("5.2.9.RELEASE");
+		assertThatPath(springFrameworkProject.resolve("5.2.x-SNAPSHOT")).isRelativeSymlinkTo("5.2.10.BUILD-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("5.3.x")).isRelativeSymlinkTo("5.3.0-RC1");
+		assertThatPath(springFrameworkProject.resolve("5.3.x-SNAPSHOT")).isRelativeSymlinkTo("5.3.0-SNAPSHOT");
+		assertThatPath(springFrameworkProject.resolve("current")).isRelativeSymlinkTo("5.3.0-RC1");
+		assertThatPath(springFrameworkProject.resolve("current-SNAPSHOT")).isRelativeSymlinkTo("5.3.0-SNAPSHOT");
 	}
 
 //	@Test
